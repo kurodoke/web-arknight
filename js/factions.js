4743996ce2723ee1d6c631c60e52e3999e81bb06
Kurodoke <ariefsatrio098@gmail.com>
2023-06-01 21:06:34 +0700

    factions update animation commit

--- js/factions.js
+++ js/factions.js
@@ -1,5 +1,16 @@
-document.querySelector(".faction-wrapper").addEventListener("wheel", function(event) {
+var sub__background = document.querySelector(".sub-scroll-animate");
+
+var wrapper__component = document.querySelector(".faction-wrapper")
+
+wrapper__component.addEventListener("wheel", function(event) {
     event.preventDefault();
     this.style.scrollBehavior = "smooth";
     this.scrollLeft += event.deltaY * 10;
+})
+
+wrapper__component.addEventListener("scroll", function(event) {
+    let maxScroll = this.scrollWidth - this.clientWidth;
+    let percentase = this.scrollLeft / maxScroll * 100;
+
+    sub__background.style.transform = `translateX(-${percentase}%)`;
 })
